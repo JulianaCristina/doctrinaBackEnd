ba6765091ce79d12ad3605e4b0b6032feeda1434
Jaqueline Neves <jaque.nevees2@gmail.com>
2020-02-04 23:52:59 -0200

    Projeto criado

---
+++ src/main/java/com/app/doctrinabackend/DoctrinabackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.doctrinabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctrinabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctrinabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/doctrinabackend/DoctrinabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.doctrinabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctrinabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
