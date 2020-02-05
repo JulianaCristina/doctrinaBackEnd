65bbcf330f37156ba4860f9fb1cbf91f15ef5c84
Jaqueline Neves <jaque.nevees2@gmail.com>
2020-02-05 00:16:29 -0200

    REST testado

---
+++ src/main/java/com/app/doctrinabackend/resources/AdministradorResource.java
@@ -0,0 +1,15 @@
+package com.app.doctrinabackend.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/adms")
+public class AdministradorResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando!";
+	}
+}
