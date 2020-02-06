f633434c3274b5b1e2709ab014fbfe3c20b23527
Jaqueline Neves <jaque.nevees2@gmail.com>
2020-02-06 01:32:59 -0200

    Banco de dados H2 e criacao automatica da BD de Adm e Prof

--- src/main/java/com/app/doctrinabackend/domain/Administrador.java
+++ src/main/java/com/app/doctrinabackend/domain/Administrador.java
@@ -1,12 +1,18 @@
 package com.app.doctrinabackend.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Administrador implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
