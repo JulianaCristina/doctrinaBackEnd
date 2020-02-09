ba240b2e4823aef87f7b1fec8c9dbadf261e7715
Jaqueline Neves <jaque.nevees2@gmail.com>
2020-02-09 12:36:04 -0200

    Tratamento de excessões em Modulo

--- src/main/java/com/app/doctrinabackend/services/ModuloService.java
+++ src/main/java/com/app/doctrinabackend/services/ModuloService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.app.doctrinabackend.domain.Modulo;
+import com.app.doctrinabackend.domain.Professor;
 import com.app.doctrinabackend.repositories.ModuloRepository;
+import com.app.doctrinabackend.services.exceptions.ObjectNotFoundException;
 
 
 @Service
@@ -16,7 +18,7 @@ public class ModuloService {
     private ModuloRepository repo; //o servico vai acessar o obj de acesso a dados que é o repository
 
     public Modulo buscar(Integer id){
-        Optional<Modulo> obj = repo.findById(id);
-        return obj.orElse(null);
+    	Optional<Modulo> obj = repo.findById(id); //o id existe retorna o objeto, se nao tiver ele retorna nulo
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Modulo.class.getName()));
     }
 }
