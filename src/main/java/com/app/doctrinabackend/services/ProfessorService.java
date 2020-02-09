4f548eabf5863c543d5cb47e5390d3e216901562
Jaqueline Neves <jaque.nevees2@gmail.com>
2020-02-09 12:29:34 -0200

    Tratamento de excessões em Professor

--- src/main/java/com/app/doctrinabackend/services/ProfessorService.java
+++ src/main/java/com/app/doctrinabackend/services/ProfessorService.java
@@ -2,6 +2,8 @@ package com.app.doctrinabackend.services;
 
 import com.app.doctrinabackend.domain.Professor;
 import com.app.doctrinabackend.repositories.ProfessorRepository;
+import com.app.doctrinabackend.services.exceptions.ObjectNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,7 @@ public class ProfessorService {
     private ProfessorRepository repo; //o servico vai acessar o obj de acesso a dados que é o repository
 
     public Professor buscar(Integer id){
-        Optional<Professor> obj = repo.findById(id);
-        return obj.orElse(null);
+        Optional<Professor> obj = repo.findById(id); //o id existe retorna o objeto, se nao tiver ele retorna nulo
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Professor.class.getName()));
     }
 }
