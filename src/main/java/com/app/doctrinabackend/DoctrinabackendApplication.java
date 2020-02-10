4856580645ec5eb6516aac74402d6a8109b5fe7c
Jaqueline Neves <jaque.nevees2@gmail.com>
2020-02-10 21:37:15 -0200

    Instancia de Modulo cadastrando automatico no BD H2

--- src/main/java/com/app/doctrinabackend/DoctrinabackendApplication.java
+++ src/main/java/com/app/doctrinabackend/DoctrinabackendApplication.java
@@ -1,5 +1,6 @@
 package com.app.doctrinabackend;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.app.doctrinabackend.domain.Administrador;
 import com.app.doctrinabackend.domain.Disciplina;
+import com.app.doctrinabackend.domain.Modulo;
 import com.app.doctrinabackend.domain.Professor;
+import com.app.doctrinabackend.domain.enums.NivelDificuldade;
+import com.app.doctrinabackend.domain.enums.Tag;
 import com.app.doctrinabackend.repositories.AdministradorRepository;
 import com.app.doctrinabackend.repositories.AtividadeRepository;
 import com.app.doctrinabackend.repositories.DisciplinaRepository;
@@ -55,21 +59,43 @@ public class DoctrinabackendApplication implements CommandLineRunner {
 		Disciplina disc3 = new Disciplina(null, "BD", "foto de BD", prof2);
 	
 		//fazer instancia de modulo, mas nao sei fazer pq tem enum, olhar como ele fez a de cliente
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
+		Modulo mod1 = new Modulo(null, "Módulo 1 de IA", "descricao do modulo 1", "link video modulo 1", "link pdf do modulo 1", Tag.FEITO, 5.0, NivelDificuldade.FACIL, 2, sdf.parse("30/09/2017 10:32"), sdf.parse("30/02/2020 10:32"), disc1);
+		Modulo mod2 = new Modulo(null, "Módulo 1 de BD", "descricao do modulo 1", "link video modulo 1", "link pdf do modulo 1", Tag.FEITO, 5.0, NivelDificuldade.FACIL, 2, sdf.parse("30/09/2017 10:32"), sdf.parse("30/02/2020 10:32"), disc2);
+		Modulo mod3 = new Modulo(null, "Módulo 2 de BD", "descricao do modulo 2", "link video modulo 2", "link pdf do modulo 2", Tag.NAO_FEITO, 9.0, NivelDificuldade.MEDIO, 2, sdf.parse("30/09/2017 10:32"), sdf.parse("30/02/2020 10:32"), disc2);
+/*
+		Atividade atividade1 = new Atividade(null, 3, 2, 0.0, 10, "foto atividade 1", mod1);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Atividade atividade2 = new Atividade(null, 4, 3, 0.0, 8, "foto atividade 2", mod2);
+		mod2.setPagamento(atividade2);
+		
+		Atividade atividade3 = new Atividade(null, 4, 3, 0.0, 8, "foto atividade 3", mod2);
+		mod3.setPagamento(atividade3);
+
+		disc1.get.addAll(Arrays.asList(mod1, mod2, mod3));
+
+		moduloRepository.save(Arrays.asList(mod1, mod23, mod));
+		atividadeRepository.save(Arrays.asList(pagto1, pagto2));
+*/
 		
 		// como nao tem a de modulo, nao da pra fazer a de atividade
 		
 		professorRepository.saveAll(Arrays.asList(prof1, prof2));
 		administradorRepository.saveAll(Arrays.asList(adm));
 		disciplinaRepository.saveAll(Arrays.asList(disc1, disc2, disc3));
+		moduloRepository.saveAll(Arrays.asList(mod1, mod2, mod3));
+
 		
 		
 		//nao da pra adc a de pergunta pq atividade nao ta pronta
 		// https://www.udemy.com/course/spring-boot-ionic/learn/lecture/8090550#questions no final
 		//Pergunta p1 = new Pergunta(null, "que cor eh a agua?", "agua eh azul", "agua eh verde", "agua eh vermelha", "letra A", 5);
 		//Pergunta p2 = new Pergunta(null, "pergunta dois", "teste um", "teste dois", "teste tres", "letra C", 10.5);
-
-		
-		
+	
 	}
 
 }
